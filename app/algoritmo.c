e81597ed0b3c85f20eb969d05206990affafdc3e
jose0212 <joseagustin.gutierrez@utp.edu.co>
2024-02-16 17:15:46 -0500

    Creacion inicial del proyecto con el codigo de la multiplicacion de matrices

---
+++ app/algoritmo.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Función para generar números aleatorios entre 1 y 5
+int generarNumeroAleatorio()
+{
+    return rand() % 5 + 1;
+}
+
+// Función para imprimir una matriz
+void imprimirMatriz(int **matriz, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Función para multiplicar dos matrices cuadradas
+void multiplicarMatrices(int **matrizA, int **matrizB, int **resultado, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            resultado[i][j] = 0;
+            for (int k = 0; k < n; k++)
+            {
+                resultado[i][j] += matrizA[i][k] * matrizB[k][j];
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    // Verificar si se proporcionó el valor de n como argumento
+    if (argc != 2)
+    {
+        printf("Uso: %s <valor_de_n>\n", argv[0]);
+        return 1;
+    }
+
+    // Obtener el valor de n desde el argumento de línea de comandos
+    printf("argv[0]: %s\nargv[1]: %s\n", argv[0], argv[1]);
+    int n = atoi(argv[1]);
+
+    // Inicializar la semilla para generar números aleatorios
+    srand(time(NULL));
+
+    // Crear matrices y reservar memoria dinámica
+    int **matrizA, **matrizB, **resultado;
+    matrizA = (int **)malloc(n * sizeof(int *));
+    matrizB = (int **)malloc(n * sizeof(int *));
+    resultado = (int **)malloc(n * sizeof(int *));
+
+    for (int i = 0; i < n; i++)
+    {
+        matrizA[i] = (int *)malloc(n * sizeof(int));
+        matrizB[i] = (int *)malloc(n * sizeof(int));
+        resultado[i] = (int *)malloc(n * sizeof(int));
+    }
+
+    // Inicializar las matrices con valores aleatorios
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrizA[i][j] = generarNumeroAleatorio();
+            matrizB[i][j] = generarNumeroAleatorio();
+        }
+    }
+
+    // Imprimir las matrices originales
+    printf("Matriz A:\n");
+    imprimirMatriz(matrizA, n);
+
+    printf("\nMatriz B:\n");
+    imprimirMatriz(matrizB, n);
+
+    // Multiplicar las matrices y almacenar el resultado en la matriz resultado
+    multiplicarMatrices(matrizA, matrizB, resultado, n);
+
+    // Imprimir la matriz resultante
+    printf("\nMatriz Resultado:\n");
+    imprimirMatriz(resultado, n);
+
+    // Liberar la memoria
+    for (int i = 0; i < n; i++)
+    {
+        free(matrizA[i]);
+        free(matrizB[i]);
+        free(resultado[i]);
+    }
+    free(matrizA);
+    free(matrizB);
+    free(resultado);
+
+    return 0;
+}
