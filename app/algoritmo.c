a389da348c250efbfe6f8b2ea3afa4ce45fa55c3
agustin-0212 <joseagustin.gutierrez@utp.edu.co>
2024-02-16 18:36:51 -0500

    Se añade comentario

--- app/algoritmo.c
+++ app/algoritmo.c
@@ -3,14 +3,18 @@
 #include <time.h>
 
 // Función para generar números aleatorios entre 1 y 5
-int generarNumeroAleatorio() {
+int generarNumeroAleatorio()
+{
     return rand() % 5 + 1;
 }
 
 // Función para imprimir una matriz
-void imprimirMatriz(int **matriz, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+void imprimirMatriz(int **matriz, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
@@ -18,20 +22,26 @@ void imprimirMatriz(int **matriz, int n) {
 }
 
 // Función para multiplicar dos matrices cuadradas
-void multiplicarMatrices(int **matrizA, int **matrizB, int **resultado, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+void multiplicarMatrices(int **matrizA, int **matrizB, int **resultado, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             resultado[i][j] = 0;
-            for (int k = 0; k < n; k++) {
+            for (int k = 0; k < n; k++)
+            {
                 resultado[i][j] += matrizA[i][k] * matrizB[k][j];
             }
         }
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // Verificar si se proporcionó el valor de n como argumento
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("Uso: %s <valor_de_n>\n", argv[0]);
         return 1;
     }
@@ -47,16 +57,19 @@ int main(int argc, char *argv[]) {
     matrizA = (int **)malloc(n * sizeof(int *));
     matrizB = (int **)malloc(n * sizeof(int *));
     resultado = (int **)malloc(n * sizeof(int *));
-    
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < n; i++)
+    {
         matrizA[i] = (int *)malloc(n * sizeof(int));
         matrizB[i] = (int *)malloc(n * sizeof(int));
         resultado[i] = (int *)malloc(n * sizeof(int));
     }
 
     // Inicializar las matrices con valores aleatorios
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             matrizA[i][j] = generarNumeroAleatorio();
             matrizB[i][j] = generarNumeroAleatorio();
         }
@@ -80,7 +93,7 @@ int main(int argc, char *argv[]) {
 
     // Calcular el tiempo transcurrido
     double tiempo_transcurrido = ((double)(fin - inicio)) / CLOCKS_PER_SEC;
-    tiempo_transcurrido = tiempo_transcurrido * 1000;//convertir a milisegundos
+    tiempo_transcurrido = tiempo_transcurrido * 1000; // convertir a milisegundos
 
     // Imprimir la matriz resultante
     printf("\nMatriz Resultado:\n");
@@ -90,7 +103,8 @@ int main(int argc, char *argv[]) {
     printf("\nTiempo de ejecucion: %.6f milisegundos\n", tiempo_transcurrido);
 
     // Liberar la memoria
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         free(matrizA[i]);
         free(matrizB[i]);
         free(resultado[i]);
